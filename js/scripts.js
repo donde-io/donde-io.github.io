9dcadce1c7e0c30f0ec000bf532be3b8da5c7527
Fabien Allanic <fabien@foodtree.com>
2013-05-30 16:23:43 -0700

    using hover event instead of click for active states

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
 	jQuery.fn.exists = function(){return this.length>0;}
 	if ($('.how-works .slides li').exists()) {
-		 $(".how-works .slides li").click(function () {
+		 $(".how-works .slides > li").hover(function () {
 			$('.how-works .slides li').removeClass("active");
 			$(this).addClass("active");
 		});
 	}
 	if ($('.info-section .box').exists()) {
-		 $(".info-section .box").click(function () {
+		 $(".info-section .box").hover(function () {
 			$('.info-section .box').removeClass("active");
 			$(this).addClass("active");
 		});
 	}
+
+    if($('.numbers .tabs .nav-tabs li').exists()){
+        $('.numbers .tabs .nav-tabs li').hover(function(event){
+            //console.log('hover number');
+            $(this).find('a').click();
+            event.stopPropagation()
+        },function(e){});
+    }
+
 	if ($('nav .btn-nav').exists()) {
 		$("nav").has(".mobile-nav").addClass("has-child");
 		$("nav a.btn-nav").click(function(){
@@ -33,14 +42,13 @@ $(document).ready(function(){
 		$('.gallery').flexslider();
 	}
 	if ($('.pricing-area .slides li').exists()) {
-		 $(".pricing-area .slides li").click(function () {
+		 $(".pricing-area .slides > li").hover(function () {
 			$('.pricing-area .slides li').removeClass("active");
 			$(this).addClass("active");
 		});
 	}
 
     function isValidEmailAddress(emailAddress) {
-        //var validEmail = new RegExp(/^[a-zA-Z0-9.!#$%&amp;'*+\-\/=?\^_`{|}~\-]+@[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*$/);
         var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
         return pattern.test(emailAddress);
     }
